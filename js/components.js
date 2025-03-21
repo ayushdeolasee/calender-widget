488cf93e8b3aefa0fa39e1bb086e6f74a4684658
Ayush Deolasee <pypdeveloper@deolasee.org>
2025-03-21 11:04:57 +0530

    adding a basic web-component from docs

--- js/components.js
+++ js/components.js
@@ -1,6 +1,21 @@
-class calender extends HTMLElement {
-    constructor(){
+class Calender extends HTMLElement {
+    constructor() {
         super();
-        this.attachShadow({mode: 'open'});
+        this.attachShadow({ mode: "open" });
+    }
+
+    connectedCallback() {
+        const shadow = this.shadowRoot;
+        const div = document.createElement("div");
+        div.classList.add("hello-world");
+        div.textContent = "Hello World!";
+        div.style.color = "orange";
+        div.style.borderWidth = "1px";
+        div.style.borderColor = "yellow";
+        div.style.borderStyle = "dashed";
+        div.style.padding = "10px";
+        shadow.appendChild(div);
     }
 }
+
+customElements.define("calender-widget", Calender);
